8635c3b317a1c490fe674226fcfa29c1c3f23506
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-05-06 17:21:24 -0300

    working prototype

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-var squares = document.getElementsByClassName("square");
+const squares = document.getElementsByClassName("square");
+const playerSelected = document.getElementById("player-selected");
+const winnerSelected = document.getElementById("winner-selected");
+const button = document.getElementById("reset");
 var player = null;
-var playerSelected = document.getElementById("player-selected");
 
 function changePlayer(value) {
   player = value;
@@ -8,6 +10,50 @@ function changePlayer(value) {
 }
 
 changePlayer("X");
+
+function isSequence(s1, s2, s3) {
+  if (
+    s1.innerHTML != "-" &&
+    s1.innerHTML == s2.innerHTML &&
+    s1.innerHTML == s3.innerHTML
+  ) {
+    s1.style.background = "#0f0";
+    s2.style.background = "#0f0";
+    s3.style.background = "#0f0";
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function hasWinner() {
+  if (isSequence(squares[0], squares[3], squares[6])) {
+    return true;
+  }
+  if (isSequence(squares[1], squares[4], squares[7])) {
+    return true;
+  }
+  if (isSequence(squares[2], squares[5], squares[8])) {
+    return true;
+  }
+  if (isSequence(squares[0], squares[1], squares[2])) {
+    return true;
+  }
+  if (isSequence(squares[3], squares[4], squares[5])) {
+    return true;
+  }
+  if (isSequence(squares[6], squares[7], squares[8])) {
+    return true;
+  }
+  if (isSequence(squares[0], squares[4], squares[8])) {
+    return true;
+  }
+  if (isSequence(squares[2], squares[4], squares[6])) {
+    return true;
+  }
+
+  return false;
+}
 
 //Each click
 for (let element of squares) {
@@ -19,6 +65,10 @@ for (let element of squares) {
     element.innerHTML = player;
     element.style.color = "#000";
 
+    if (hasWinner()) {
+      winnerSelected.innerHTML = player;
+    }
+
     if (player == "X") {
       changePlayer("O");
     } else {
@@ -26,3 +76,15 @@ for (let element of squares) {
     }
   });
 }
+
+//Reset button
+button.addEventListener("click", function () {
+  winnerSelected.innerHTML = "";
+  changePlayer("X");
+
+  for (let element of squares) {
+    element.innerHTML = "-";
+    element.style.color = "#eee";
+    element.style.background = "#eee";
+  }
+});
